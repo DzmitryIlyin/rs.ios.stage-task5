a5efb1a4b183d22e0823f1b4f5762a3cb962075a
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-06-12 14:50:06 +0300

    Exercise_5_1_solved

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,24 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        var result = 0
+        if prices.count == 0 {return result}
+        
+        outerLoop: for i in 0..<prices.count {
+            var tempMax = 0
+            innerLoop: for j in i+1..<prices.count {
+                if prices[i] > prices[j] {
+                    continue innerLoop
+                } else if (prices[j] - prices[i] > tempMax){
+                    tempMax = prices[j] - prices[i]
+                }
+            }
+            result += tempMax
+        }
+        
+        return result
     }
 }
