a046ed1dab986a0d949502bda0ba51973dc8e687
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-06-14 20:18:29 +0300

    Exercise_5_3_solved

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,29 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        let foodsKnapsack = fillKnapsack(items: foods)
+        let drinksKnapsack = fillKnapsack(items: drinks)
+        
+        var maxKilometers = 0
+        for i in 1...maxWeight {
+            let min = min(foodsKnapsack[foodsKnapsack.count - 1][i], drinksKnapsack[drinksKnapsack.count - 1][maxWeight - i])
+            maxKilometers = min > maxKilometers ? min : maxKilometers
+        }
+        return maxKilometers
+    }
+    
+    func fillKnapsack(items: Array<Supply>) -> Array<Array<Int>> {
+        let valuesCount = items.count
+        var t = Array(repeating: Array(repeating: 0,count: maxWeight+1), count: valuesCount+1)
+        for i in 1 ..< valuesCount+1 {
+            for j in 1 ..< maxWeight+1 {
+                if items[i-1].weight <= j {
+                    t[i][j] = max(t[i-1][j-items[i-1].weight] + items[i-1].value, t[i-1][j])
+                } else {
+                    t[i][j] = t[i-1][j]
+                }
+            }
+        }
+        return t
     }
 }
